734404a73d512f1f4dbb7f308eff56df6093fc90
BisonJeedom <bisondefer@gmail.com>
2024-02-19 08:41:48 +0100

    Ajustement des valeurs de zoom et de la couleur de la barre

--- desktop/modal/modal.diagrelationnel.php
+++ desktop/modal/modal.diagrelationnel.php
@@ -17,62 +17,59 @@
 */
 
 if (!isConnect('admin')) {
-    throw new Exception('{{401 - Accès non autorisé}}');
+  throw new Exception('{{401 - Accès non autorisé}}');
 }
 
 if (substr_compare(init('src'), '.png', -strlen('.png')) !== 0) {
-    throw new Exception('{{401 - Accès non autorisé}}');
+  throw new Exception('{{401 - Accès non autorisé}}');
 }
 if (strpos(init('src'), 'diagrelationnel') === false) {
-    throw new Exception('{{401 - Accès non autorisé}}');
+  throw new Exception('{{401 - Accès non autorisé}}');
 }
 ?>
-  
+
 <div class="slidecontainer" style="width: 200px;">
-  <input type="range" min="50" max="350" value="75" class="slider" id="myRange" style="background-color: var(--btn-default-color);">
+  <input type="range" min="30" max="600" value="100" class="slider" id="myRange" style="background-color: #a7abab !important;">
 </div>
 
 <?php
-echo '<center><img id="img-id" class="img-responsive" src="' . init('src') . '" style="background: var(--objectTxt-color); width: 75%; margin-top: 15px" /></center>';
+echo '<center><img id="img-id" class="img-responsive" src="' . init('src') . '" style="background: var(--objectTxt-color); max-width: 600%; width: 100%; margin-top: 15px" /></center>';
 ?>
 
 <style>
-  
-.slider {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 100%;
-  height: 15px;
-  border-radius: 5px;  
-  outline: none;
-  opacity: 0.7;
-  -webkit-transition: .2s;
-  transition: opacity .2s;
-}
+  .slider {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 100%;
+    height: 15px;
+    border-radius: 5px;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+  }
 
-.slider::-webkit-slider-thumb {
-  -webkit-appearance: none;  
-  appearance: none;
-  width: 25px;
-  height: 25px;
-  border-radius: 50%; 
-  background: var(--al-info-color);
-  cursor: pointer;
-}
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: #15ebeb !important;
+    cursor: pointer;
+  }
 
-.slider::-moz-range-thumb {
-  width: 25px;
-  height: 25px;
-  border-radius: 50%;
-  background: var(--al-info-color);
-  cursor: pointer;
-}
-
+  .slider::-moz-range-thumb {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background: var(--al-info-color);
+    cursor: pointer;
+  }
 </style>
 
 <script>
   var slider = document.getElementById("myRange");
-  // Update the current slider value (each time you drag the slider handle)
   slider.oninput = function() {
     document.getElementById("img-id").style.width = this.value + "%";
   }
